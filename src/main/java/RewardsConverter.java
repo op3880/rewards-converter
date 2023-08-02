cf22b6e6ea6c38e237f94a8d5f7c72c13a79e99d
Omar Daas <daasomar@yahoo.com>
2023-08-02 13:58:26 -0400

    Task 1 Corrected

--- src/main/java/RewardsConverter.java
+++ src/main/java/RewardsConverter.java
@@ -18,18 +18,32 @@ public class RewardsConverter {
         System.out.println("$" + input_value + " is worth " + rewardsValue.getMilesValue() + " miles");
     }
 
-    static class RewardValue {
-        double value;
-        public RewardValue(double v){
-            value =v;
+    public static class RewardValue {
+        private final double cashValue;
+        public static final double MILES_TO_CASH_CONVERSION_RATE = 0.0035;
+
+        public RewardValue(double cashValue) {
+            this.cashValue = cashValue;
         }
 
-        public double getCashValue(){
-            return (value*.0035);
+        public RewardValue(int milesValue) {
+            this.cashValue = convertToCash(milesValue);
         }
 
-        public double getMilesValue(){
-            return (value/.0035);
+        private static int convertToMiles(double cashValue) {
+            return (int) (cashValue / MILES_TO_CASH_CONVERSION_RATE);
+        }
+
+        private static double convertToCash(int milesValue) {
+            return milesValue * MILES_TO_CASH_CONVERSION_RATE;
+        }
+
+        public double getCashValue() {
+            return cashValue;
+        }
+
+        public int getMilesValue() {
+            return convertToMiles(this.cashValue);
         }
     }
 }
